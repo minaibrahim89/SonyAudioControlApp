4df32b4b4ec58af56478713db5a07aee6b9ef14e
Mina Ibrahim <eng.mina.hanna@hotmail.com>
2020-11-16 00:50:12 +0100

    Use socket directly for discovering devices.

--- SonyAudioControl/Services/Discovery/DeviceFinder.cs
+++ SonyAudioControl/Services/Discovery/DeviceFinder.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
-using Rssdp;
-using SonyAudioControl.Extensions;
 using SonyAudioControl.Model;
 
 namespace SonyAudioControl.Services.Discovery
@@ -22,12 +22,52 @@ namespace SonyAudioControl.Services.Discovery
 
         public async Task<IEnumerable<DeviceDescription>> SearchForUpnpDevicesAsync()
         {
-            using (var deviceLocator = new SsdpDeviceLocator())
-            {
-                var foundDevices = await deviceLocator.SearchAsync("urn:schemas-sony-com:service:ScalarWebAPI:1");
+            using var multicastSocket = CreateMulticastSocket(out var endPoint);
+            const string searchString = @"M-SEARCH * HTTP/1.1
+HOST:239.255.255.250:1900
+MAN: ""ssdp:discover""
+ST:urn:schemas-sony-com:service:ScalarWebAPI:1
+MX:10
 
-                return await Task.WhenAll(foundDevices.Select(d => GetDeviceDescription(d.DescriptionLocation)).Where(x => x != null));
+";
+            multicastSocket.SendTo(Encoding.UTF8.GetBytes(searchString), SocketFlags.None, endPoint);
+
+            var receiveBuffer = new byte[64000];
+
+            var deviceUrls = new List<Uri>();
+
+            for (var i = 0; i < 10; ++i)
+            {
+                if (multicastSocket.Available == 0)
+                {
+                    await Task.Delay(1000);
+                    continue;
+                }
+
+                var receivedBytes = await multicastSocket.ReceiveAsync(receiveBuffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(receiveBuffer, 0, receivedBytes);
+                var deviceUrl = response.Split(Environment.NewLine).Single(line => line.StartsWith("LOCATION")).Substring(9).Trim();
+                deviceUrls.Add(new Uri(deviceUrl));
+                i = 0;
             }
+
+            return await Task.WhenAll(deviceUrls.Select(GetDeviceDescription).Where(x => x != null));
+        }
+
+        private Socket CreateMulticastSocket(out IPEndPoint multicastEndpoint)
+        {
+            var LocalEndPoint = new IPEndPoint(IPAddress.Any, 60000);
+            multicastEndpoint = new IPEndPoint(IPAddress.Parse("239.255.255.250"), 1900);
+
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpSocket.Bind(LocalEndPoint);
+            udpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(multicastEndpoint.Address, IPAddress.Any));
+            udpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+            udpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastLoopback, true);
+
+            return udpSocket;
         }
 
         private async Task<DeviceDescription> GetDeviceDescription(Uri descriptionLocation)
